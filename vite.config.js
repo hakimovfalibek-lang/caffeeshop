904a054fa21828a096112395d9a5c187a0ce386f
hakimovfalibek-lang <hakimovfalibek@gmail.com>
2026-09-17 12:27:12 +0500

    fix render host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     host: "0.0.0.0",
     port: 3000,
@@ -11,5 +12,10 @@ export default defineConfig({
     hmr: {
       port: 3000,
     },
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: ["caffeeshop-1.onrender.com"],
   },
 });
